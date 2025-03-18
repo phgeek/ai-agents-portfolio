cba631e4f5f6c71ca39c67369b3a13a05b663b91
Phillippe Caetano <ph.ufeb@gmail.com>
2025-03-18 19:40:42 -0300

    feat(backend): database schema complete

---
+++ backend/prisma/migrations/20250318223450_relation_technologies_projects/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "_ProjectToTechnology" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL,
+
+    CONSTRAINT "_ProjectToTechnology_AB_pkey" PRIMARY KEY ("A","B")
+);
+
+-- CreateIndex
+CREATE INDEX "_ProjectToTechnology_B_index" ON "_ProjectToTechnology"("B");
+
+-- AddForeignKey
+ALTER TABLE "_ProjectToTechnology" ADD CONSTRAINT "_ProjectToTechnology_A_fkey" FOREIGN KEY ("A") REFERENCES "projects"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ProjectToTechnology" ADD CONSTRAINT "_ProjectToTechnology_B_fkey" FOREIGN KEY ("B") REFERENCES "technologies"("id") ON DELETE CASCADE ON UPDATE CASCADE;
